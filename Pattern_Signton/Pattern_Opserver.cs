15a5675f628260a8144cfa588a4c34c94e8d19d8
from983002 <from983002@gmail.com>
2016-03-09 15:13:53 +0900

    2016030905

---
+++ Pattern_Signton/Pattern_Opserver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Signton
+{
+    /*
+    public Form1()
+        {
+            InitializeComponent();
+
+            CallBackTest cbt = new CallBackTest();
+            cbt.onComplete(Image_File_Delete_Test);
+        }
+
+   
+
+    public class CallBackTest
+    {
+        //콜백을 전달할 delegate 선언 (인수 전달/)
+        public delegate void onCompleteDele(int i);
+
+        public void onComplete(onCompleteDele _callCompleteMethod)
+        {
+            Timer t = new Timer();
+            t.Interval = 1000;
+            t.Start();
+            int count = 0;
+
+            t.Tick += delegate {
+                if (count == 2)
+                    //!!콜백발생
+                    _callCompleteMethod(count);
+                ++count;
+            };
+        }
+    } 
+    */
+
+    class Pattern_Opserver
+    {
+        
+    }
+}
