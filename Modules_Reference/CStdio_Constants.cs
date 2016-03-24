d3fc177b933562e88bc3f2944b6a16f1748fce67
from983002 <from983002@gmail.com>
2016-03-24 16:53:10 +0900

    2016032403

---
+++ Modules_Reference/CStdio_Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//C++의 Define 과 같은 효과를 가지는 클래스 상수를 선언해서 사용한다.
+//CStdio_Constants.Pi 로 사용하며 된다.
+namespace Modules_Reference
+{
+    static class CStdio_Constants
+    {
+        public const double Pi = 3.14159;//3.1415926535897932384;
+    }
+}
